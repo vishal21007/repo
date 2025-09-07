0ccffbdd3eb7054fd45214f99eda282e32528851
vishal21007 <vishalrathor2109@gmail.com>
2025-09-07 07:19:37 +0000

    update

---
+++ Dictionary/1.py
@@ -0,0 +1,19 @@
+# declare a dictionary and print values
+
+d1 = { 1:'Vishal', 2:'budi',3:'hello',4:'redy' }
+l=[(1,'amit'),(2,"billi")]
+d1[5]='hi'
+d2=dict(l)
+print(d1)
+for keys,values in d1.items():
+    print(keys,values)
+print(d2)
+
+'''print(d1)
+d1[2]='dog'
+print(d1)
+del d1[3]
+print(d1)
+d1.pop(2)
+print(d1)
+'''
---
+++ Dictionary/2.py
@@ -0,0 +1,10 @@
+#world frquenct counter
+txt="python is a easy and powerful language"
+
+d1={}
+for i in txt.split():
+    c=len(i)
+    d1[i]=c
+
+print(d1)
+        
