74a9ab9790dfaad91d5bd1c401ae679fdce3c3e5
vishal21007 <vishalrathor2109@gmail.com>
2025-11-02 07:46:31 +0000

    update

--- assement_1/38.py
+++ assement_1/38.py
@@ -2,4 +2,21 @@
 # last characters are same from a given list of strings.
 
 lst = ['aba','xyz','121','bob','112'] # sample list
+count = 0
+for i in lst:
+    if len(i) >= 2 and i[0]==i[-1]:
+        print(i)
+        count = count + 1
+print('Total Count : ',count)
 
+
+'''
+count=0
+count1=0
+for i in range(len(lst)):
+    if len(lst[i]) > 2 :
+        count=count+1
+    for j in lst[i]:
+        print(lst[j])
+print("There are",count,"string length more than 2 ")    
+'''
