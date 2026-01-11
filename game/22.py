2b816a881300b62015a6d3b38905a42c950d6bef
vishal21007 <vishalrathor2109@gmail.com>
2026-01-11 07:39:07 +0000

    update

--- game/22.py
+++ game/22.py
@@ -72,6 +72,8 @@ elif game==2:
     draw=0
     for i in range(5):
         print("\n---Round - ",i+1,"---\n")
+        print("Your current balace :",user_balance)
+        print("Sysem current balance :",system_balance,"\n")
         while True:
             user_bet=int(input("Enter your bet amount (min:100 - max:1000) : "))
             if user_bet<100 or user_bet > 1000:
@@ -81,9 +83,12 @@ elif game==2:
                 break
         system_bet=random.randint(100,250)
         print("system bet : ",system_bet)
+        user_balance=user_balance-user_bet
+        system_balance=system_balance-system_bet
         system=[]
         user=[]
         for i in range(3):
+            
             system_symbol=random.randint(1,7)
             user_symbol=random.randint(1,7)
             system.append(system_symbol)
@@ -93,47 +98,70 @@ elif game==2:
         print("System slots : [",symbols[system[0]]," | ",symbols[system[1]]," | ",symbols[system[2]]," ]")
     
         if user==system:
-            print("The round has been drawed !!!")
+            print("The round has been drawed 2!!!")
             draw=draw+1
-        elif  user[0]!=user[1]!=user[2] and  system[0]!=system[1]!=system[2]:
-            print("The round has been drawed !!!")
-            draw=draw+1
-        #
         else:
             if user[0]==user[1]==user[2] and user.sort() != system.sort():
                 print("User wins the round \nwins bet into 10X\nAmount won =",user_bet*10)
                 user_balance=user_balance+user_bet*10
-                system_balance=system_balance-system_bet
+        #    system_balance=system_balance-system_bet
                 win=win+1
             elif   system[0]==system[1]==system[2] and user.sort() != system.sort():
                 print("System wins the round \nwins bet into 10X\nAmount won =",system_bet*10)
-                user_balance=user_balance-user_bet
+       #      us er_balance=user_balance-user_bet
                 system_balance=system_balance+system_bet*10
                 lose=lose+1
             elif user[0]!=user[1]!=user[2] and system[0]==system[1]==system[2]:
                 print("System wins the round \nwins bet into 10X\nAmount won =",system_bet*10)
-                user_balance=user_balance-user_bet
+      #      user_balance=user_balance-user_bet
                 system_balance=system_balance+system_bet*10
                 lose=lose+1
             elif system[0]!=system[1]!=system[2] and user[0]==user[1]==user[2]:
                 print("User wins the round \nwins bet into 10X\nAmount won =",user_bet*10)
                 user_balance=user_balance+user_bet*10
-                system_balance=system_balance-system_bet
+     #       system_balance=system_balance-system_bet
                 win=win+1
             elif (user[0] == user[1] or user[1] == user[2] or user[0] == user[2]) and system[0]!=system[1]!=system[2]:
                 print("User wins the round \nwins bet into 3X\nAmount won =",user_bet*3)
                 user_balance=user_balance+user_bet*3
-                system_balance=system_balance-system_bet
+    #        system_balance=system_balance-system_bet
                 win=win+1
             elif (system[0] == system[1] or system[1] == system[2] or system[0] == system[2]) and user[0]!=user[1]!=user[2]:
                 print("System wins the round \nwins bet into 3X\nAmount won",system_bet*3)
-                user_balance=user_balance-user_bet
+   #          user_balance=user_balance-user_bet
                 system_balance=system_balance+system_bet*3
                 lose=lose+1
+            elif   user[0]!=user[1]!=user[2] and  system[1]!=system[2]!=system[0]:
+                print("The round has been losed by both System and User!!!")
+ #       user_balance=user_balance-user_bet
+#        system_balance=system_balance-system_bet        #
+            elif  (user[0]==user[1]==user[2]==6 or user[0]==user[1]==user[2]==7) and system[0]==system[1]==system[2]:
+                print("User wins the jackpot \nBet into 50X \nAmount won = :",user_bet*50)
+                user_balance=user_balance+user_bet*50
+                system_balance=system_balance+system_bet*10
+            elif (user[0]==user[1]==user[2]==6 or user[0]==user[1]==user[2]==7) and (system[0]!=system[1]!=system[2]):
+                print("User wins the jackpot \nBet into 50X \nAmount won = :",user_bet*50)
+                user_balance=user_balance+user_bet*50
+  #          system_balance=system_balance-system_bet
+            elif (user[0]==user[1]==user[2]==6 or user[0]==user[1]==user[2]==7) and (system[0] == system[1] or system[1] == system[2] or system[0] == system[2]):
+                print("User wins the jackpot \nBet into 50X \nAmount won = :",user_bet*50)
+                user_balance=user_balance+user_bet*50
+                system_balance=system_balance+system_bet*3
+            elif (system[0]==system[1]==system[2]==6 or system[0]==system[1]==system[2]==7) and user[0]==user[1]==user[2]:
+                print("Sysrem wins the jackpot \nBet into 50X \nAmount won = :",system_bet*50)
+                system_balance=system_balance+system_bet*50
+                user_balance=user_balance+user_bet*10
+            elif (system[0]==system[1]==system[2]==6 or system[0]==system[1]==system[2]==7) and  user[0]!=user[1]!=user[2]:
+                print("Sysrem wins the jackpot \nBet into 50X \nAmount won = :",system_bet*50)
+                system_balance=system_balance+system_bet*50
+ #           user_balance=user_balance-user_bet
+            elif (system[0]==system[1]==system[2]==6 or system[0]==system[1]==system[2]==7) and (user[0] == user[1] or user[1] == user[2] or user[0] == user[2]):
+                print("Sysrem wins the jackpot \nBet into 50X \nAmount won = :",system_bet*50)
+                system_balance=system_balance+system_bet*50
+                user_balance=user_balance+user_bet*3
             else:
-                print("The round has been drawed !!!")
+                print("The round has been drawed 1!!!")
                 draw=draw+1
-            
     print("--- FINAL RESULT ---\n")
     print("Rounds won by user : ",win)
     print("Rounds won by system : ",lose)
---
+++ game/24.py
@@ -0,0 +1,6 @@
+count=0
+for i in range(3):
+    for i in range(3):
+        print("|",count,end=" ")
+        count=count+1
+    print()
